30ccc4121ff536fac30c35c5dcc47bd18ce9e1ce
Fernando Blanch Calvete <fernandobc@ext.inditex.com>
2026-09-16 13:27:28 +0200

    update 2025.1.3

--- src/main/java/com/example/demoStreamKafka/config/ConfigurationApp.java
+++ src/main/java/com/example/demoStreamKafka/config/ConfigurationApp.java
@@ -2,51 +2,25 @@ package com.example.demoStreamKafka.config;
 
 import com.example.demoStreamKafka.dto.ProductCompleteDTO;
 import com.example.demoStreamKafka.dto.ProductSimpleDTO;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.slf4j.Logger;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.cloud.function.json.JacksonMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 
 import java.util.function.Consumer;
 
 @Configuration
 public class ConfigurationApp {
 
-    private static final Logger LOG = org.slf4j.LoggerFactory.getLogger(ConfigurationApp.class);
+	private static final Logger LOG = org.slf4j.LoggerFactory.getLogger(ConfigurationApp.class);
 
-    @Bean
-    public Consumer<ProductSimpleDTO> simpleConsumer() {
-        return s -> {
-            LOG.info("Received simple Id: {}", s.getId());
-        };
-    }
+	@Bean
+	public Consumer<ProductSimpleDTO> simpleConsumer() {
+		return s -> LOG.info("Received simple Id: {}", s.getId());
+	}
 
-    @Bean
-    public Consumer<ProductCompleteDTO> completeConsumer() {
-        return s -> {
-            LOG.info("Received complete Id: {}", s.getId());
-        };
-    }
-
-    @Configuration
-    @ConditionalOnProperty(value = "demo.jackson.mapper.enabled", havingValue = "true", matchIfMissing = true)
-    public static class JacksonConfiguration {
-
-        @Bean
-        @Primary
-        public JacksonMapper jacksonMapper(final ObjectMapper objectMapper) {
-            objectMapper.registerModule(new JavaTimeModule());
-            objectMapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
-            objectMapper.configure(DeserializationFeature.FAIL_ON_TRAILING_TOKENS, true);
-            return new JacksonMapper(objectMapper);
-        }
-
-    }
+	@Bean
+	public Consumer<ProductCompleteDTO> completeConsumer() {
+		return s -> LOG.info("Received complete Id: {}", s.getId());
+	}
 
 }
